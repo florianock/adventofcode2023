831d741b32408b1c2d274709cfb1f4aa8b22a777
Florian <florian@zocalo-ict.nl>
2023-12-08 22:05:05 +0100

    Refactored code a bit

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,45 +1,6 @@
 fun main() { // --- Day 7: Camel Cards ---
-    data class Game(val hand: String, val bid: Int, val withJokers: Boolean) : Comparable<Game> {
-        val type = if (withJokers) substituteJokers() else typeFromHand(this.hand)
-
-        private fun rankType(t: List<Int> = this.type): Int = 5 - t.size + t.first()
-
-        private fun getHandComparator(): String =
-            hand.map { c -> getCardValue(c) }.joinToString { i -> i.toString(14) }
-
-        private fun substituteJokers(): List<Int> {
-            return if (hand.contains('J')) {
-                val possibleCards = arrayOf('A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2')
-                possibleCards.map { c -> typeFromHand(hand.replace('J', c)) }.maxBy(::rankType)
-            } else {
-                typeFromHand(hand)
-            }
-        }
-
-        private fun typeFromHand(hand: String): List<Int> =
-            hand.groupBy { it }.map { (_, v) -> v.size }.sortedDescending()
-
-        private fun getCardValue(x: Char): Int = when {
-            x.isDigit() -> x.toString().toInt() - 1
-            x == 'T' -> 9
-            x == 'J' -> if (withJokers) 0 else 10
-            x == 'Q' -> 11
-            x == 'K' -> 12
-            x == 'A' -> 13
-            else -> throw IllegalArgumentException("Unknown card: $x")
-        }
-
-        override fun compareTo(other: Game): Int =
-            compareValuesBy(this, other, { it.rankType() }, { it.getHandComparator() })
-    }
-
-    fun parse(input: List<String>, withJokers: Boolean): List<Game> {
-        return input.map { l ->
-            l.split(' ').let { arr ->
-                Game(arr[0], arr[1].toInt(), withJokers)
-            }
-        }
-    }
+    fun parse(input: List<String>, withJokers: Boolean): List<CamelCardGame> =
+        input.map { l -> l.split(' ').let { CamelCardGame(it[0], it[1].toInt(), withJokers) } }
 
     fun part1(input: List<String>): Int = parse(input, false)
         .sorted()
@@ -59,4 +20,37 @@ fun main() { // --- Day 7: Camel Cards ---
     val input = readInput("Day07")
     part1(input).println()
     part2(input).println()
+}
+
+data class CamelCardGame(private val cards: String, val bid: Int, private val withJokers: Boolean) :
+    Comparable<CamelCardGame> {
+    private val type = if (withJokers) substituteJokers() else getType(cards)
+
+    companion object {
+        val PossibleCards = arrayOf('A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2')
+    }
+
+    private fun rank(t: List<Int> = this.type): Int = 5 - t.size + t.first()
+
+    private fun hand(): String = cards.map(::getBase14Value).joinToString("")
+
+    private fun substituteJokers(): List<Int> =
+        if (cards.contains('J'))
+            PossibleCards.map { c -> getType(cards.replace('J', c)) }.maxBy(::rank)
+        else
+            getType(cards)
+
+    private fun getType(hand: String): List<Int> = hand.groupBy { it }.map { (_, v) -> v.size }.sortedDescending()
+
+    private fun getBase14Value(x: Char): Char = when {
+        x.isDigit() -> x - 1
+        x == 'T' -> '9'
+        x == 'J' -> if (withJokers) '0' else 'a'
+        x == 'Q' -> 'b'
+        x == 'K' -> 'c'
+        x == 'A' -> 'd'
+        else -> throw IllegalArgumentException("Unknown card: $x")
+    }
+
+    override fun compareTo(other: CamelCardGame): Int = compareValuesBy(this, other, { it.rank() }, { it.hand() })
 }
