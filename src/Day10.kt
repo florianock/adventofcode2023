dd97cf29d3cea70afe90c1bf77563cc0d568bc22
Florian <florian@zocalo-ict.nl>
2023-12-11 21:21:09 +0100

    Day 10 halfway done

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,18 +1,161 @@
+enum class Direction { North, East, South, West }
+
 fun main() {
+    val visuals = true
+
+    // verticaal: 7F; 7|; |F; J|; |L; JF; JL; 7L
+    // horizontaal: J; L; J; L; -; L; J; -
+    //              -; -; 7; 7; 7; F; F; F
+    val definitions = hashMapOf(
+        Direction.North to arrayOf('7', '|', 'F'),
+        Direction.East to arrayOf('J', '-', '7'),
+        Direction.South to arrayOf('L', '|', 'J'),
+        Direction.West to arrayOf('F', '-', 'L')
+    )
+
+    fun getNextPosition(pos: Pair<Int, Int>, step: Char, heading: Direction): Pair<Pair<Int, Int>, Direction>? {
+        return when (heading) {
+            Direction.North ->
+                when (step) {
+                    '7' -> Pair(Pair(pos.first, pos.second - 1), Direction.West)    // heading - 1  West
+                    '|' -> Pair(Pair(pos.first - 1, pos.second), heading)           // continue     North
+                    'F' -> Pair(Pair(pos.first, pos.second + 1), Direction.East)    // heading + 1  East
+                    else -> null
+                }
+
+            Direction.East ->
+                when (step) {
+                    'J' -> Pair(Pair(pos.first - 1, pos.second), Direction.North)   // heading - 1  North
+                    '-' -> Pair(Pair(pos.first, pos.second + 1), heading)           // continue     East
+                    '7' -> Pair(Pair(pos.first + 1, pos.second), Direction.South)   // heading + 1  South
+                    else -> null
+                }
+
+            Direction.South ->
+                when (step) {
+                    'J' -> Pair(Pair(pos.first, pos.second - 1), Direction.West)    // heading + 1  West
+                    '|' -> Pair(Pair(pos.first + 1, pos.second), heading)           // continue     South
+                    'L' -> Pair(Pair(pos.first, pos.second + 1), Direction.East)    // heading - 1  East
+                    else -> null
+                }
+
+            Direction.West ->
+                when (step) {
+                    'L' -> Pair(Pair(pos.first - 1, pos.second), Direction.North)   // heading + 1  North
+                    '-' -> Pair(Pair(pos.first, pos.second - 1), heading)           // continue     West
+                    'F' -> Pair(Pair(pos.first + 1, pos.second), Direction.South)   // heading - 1  South
+                    else -> null
+                }
+        }
+    }
+
+    fun getGridAndStartPosition(input: List<String>): Pair<Array<Array<Char>>, Pair<Int, Int>> {
+        val startRow = input.indexOfFirst { l -> l.contains('S') }
+        val startCol = input[startRow].indexOf('S')
+        val grid = Array(input.size) { i -> Array(input[i].length) { j -> input[i][j] } }
+        return Pair(grid, Pair(startRow, startCol))
+    }
+
+    fun loopAround(start: Pair<Int, Int>, heading: Direction, input: List<String>): Array<Pair<Int, Int>>? {
+        val nextPos = when (heading) {
+            Direction.North -> Pair(start.first - 1, start.second)
+            Direction.East -> Pair(start.first, start.second + 1)
+            Direction.South -> Pair(start.first + 1, start.second)
+            Direction.West -> Pair(start.first, start.second - 1)
+        }
+
+        var steps = arrayOf(start, nextPos)
+        var pos = nextPos
+        var currentHeading = heading
+        while (pos != start) {
+            val next = getNextPosition(pos, input[pos.first][pos.second], currentHeading) ?: return null
+            steps += next.first
+            pos = next.first
+            currentHeading = next.second
+        }
+        return steps
+    }
+
+    fun findLoop(start: Pair<Int, Int>, input: List<String>): Array<Pair<Int, Int>> {
+        var loop = emptyArray<Pair<Int, Int>>()
+        for (heading in Direction.entries) {
+            loop = loopAround(start, heading, input) ?: continue
+            break
+        }
+        return loop
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val (grid, start) = getGridAndStartPosition(input)
+        val loop = findLoop(start, input).toSet()
+        if (visuals) print2d(grid, mapOf(Colors.Yellow to loop))
+        return loop.size / 2
+    }
+
+    fun findUnflooded(gridWithHeight: Array<Array<Pair<Char, Int?>>>): Pair<Int, Int>? {
+        val startRow = gridWithHeight.indexOfFirst { r -> r.any { c -> c.second == null } }
+        if (startRow < 0) return null
+        val startCol = gridWithHeight[startRow].indexOfFirst { c -> c.second == null }
+        return Pair(startRow, startCol)
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val (grid, start) = getGridAndStartPosition(input)
+        val loop = findLoop(start, input).toSet()
+        val gridWithHeight = grid.mapIndexed { i, row ->
+            row.mapIndexed { j, cell ->
+                val height = if (loop.contains(Pair(i, j))) Int.MAX_VALUE else null
+                Pair(cell, height)
+            }.toTypedArray()
+        }.toTypedArray()
+
+        var height = 1
+        while (true) {
+            val (startRow, startCol) = findUnflooded(gridWithHeight) ?: break
+            gridWithHeight.flood(Pair(startRow, startCol), height)
+            height++
+        }
+
+        val loopRows = loop.minOf { (r, _) -> r }..loop.maxOf { (r, _) -> r }
+        val loopCols = loop.minOf { (_, c) -> c }..loop.maxOf { (_, c) -> c }
+
+        val outsides = mutableSetOf<Int>()
+        var boxedIn = mutableSetOf<Pair<Int, Int>>()
+        for (r in gridWithHeight.indices) {
+            for (c in gridWithHeight[r].indices) {
+                if (gridWithHeight[r][c].second != null && gridWithHeight[r][c].second!! < Int.MAX_VALUE) {
+                    if (loopRows.contains(r) && loopCols.contains(c)) {
+                        boxedIn.add(Pair(r, c))
+                    } else {
+                        outsides.add(gridWithHeight[r][c].second!!)
+                    }
+                }
+            }
+        }
+        boxedIn = boxedIn.filter { (i, j) -> !outsides.contains(gridWithHeight[i][j].second) }.toMutableSet()
+        if (visuals) print2d(
+            gridWithHeight.map { r -> r.map { c -> c.first }.toTypedArray() }.toTypedArray(),
+            mapOf(Colors.Yellow to loop, Colors.Green to boxedIn)
+        )
+        // slide: zien of groepen verbonden zijn?
+        return boxedIn.also(::println).size
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day0x_test")
-    check(part1(testInput) == 1)
-    check(part2(testInput) == 1)
-
-    val input = readInput("Day0x")
+    val testInput1 = readInput("Day10_test1")
+    check(part1(testInput1) == 4)
+    val testInput2 = readInput("Day10_test2")
+    check(part1(testInput2) == 8)
+    val input = readInput("Day10")
     part1(input).println()
-    part2(input).println()
+
+//    val testInput3 = readInput("Day10_test3")
+//    check(part2(testInput3) == 4)
+//    val testInput4 = readInput("Day10_test4")
+//    check(part2(testInput4) == 4)
+//    val testInput5 = readInput("Day10_test5")
+//    check(part2(testInput5) == 8)
+//    val testInput6 = readInput("Day10_test6")
+//    check(part2(testInput6) == 10)
+    part2(input).println() // 794 too high
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -3,6 +3,7 @@ import java.security.MessageDigest
 import kotlin.io.path.Path
 import kotlin.io.path.readLines
 import kotlin.io.path.readText
+import kotlin.math.abs
 import kotlin.math.pow
 
 /**
@@ -46,10 +47,22 @@ fun isAdjacent(p1: Pair<Int, Int>, p2: Pair<Int, Int>) = p1 in neighbors(p2)
 enum class Colors { Red, Green, Yellow }
 
 /**
+ * Print 2d Array of Array<Char>
+ */
+fun print2d(matrix: Array<Array<Char>>, colors: Map<Colors, Set<Pair<Int, Int>>>?) {
+    val m = Array(matrix.size) { r ->
+        CharArray(matrix[r].size) { c ->
+            matrix[r][c]
+        }
+    }
+    print2d(m, colors)
+}
+
+/**
  * Print 2D matrix to console
  */
 fun print2d(
-    matrix: Array<Array<Char>>,
+    matrix: Array<CharArray>,
     colors: Map<Colors, Set<Pair<Int, Int>>>?
 ) {
     val red = "\u001b[31m"
@@ -118,3 +131,63 @@ fun List<Long>.leastCommonMultiple(): Long {
         leastCommonMultiple(acc, n)
     }
 }
+
+/**
+ * Flood Fill Algorithm in a Collection, starting at the given start position.
+ */
+fun <E> Array<Array<Pair<E, Int?>>>.flood(pos: Pair<Int, Int>, target: Int) {
+    val maxRow = this.size - 1
+    val maxCol = this[0].size - 1
+
+    if (pos.first < 0 || pos.first > maxRow || pos.second < 0 || pos.second > maxCol)
+        return
+
+    if (this[pos.first][pos.second].second == Int.MAX_VALUE || this[pos.first][pos.second].second != null)
+        return
+
+    this[pos.first][pos.second] = Pair(this[pos.first][pos.second].first, target)
+
+    this.flood(Pair(pos.first + 1, pos.second), target)
+    this.flood(Pair(pos.first - 1, pos.second), target)
+    this.flood(Pair(pos.first, pos.second + 1), target)
+    this.flood(Pair(pos.first, pos.second - 1), target)
+}
+
+/**
+ * Transpose a 2D CharArray.
+ */
+fun Array<CharArray>.transpose(): Array<CharArray> {
+    val cols = this[0].size
+    val rows = this.size
+    return Array(cols) { j -> CharArray(rows) { i -> this[i][j] } }
+}
+
+/**
+ * Get all pairs of a Collection.
+ */
+fun <T> Iterable<T>.allPairs(): Set<Pair<T, T>> {
+    val result = this.runningFoldIndexed(arrayListOf<Pair<T, T>>()) { idx, acc, _ ->
+        val arr = this.toList()
+        for (i in (idx + 1)..<arr.size) {
+            acc.add(Pair(arr[idx], arr[i]))
+        }
+        acc
+    }.flatten().toSet()
+    return result
+}
+
+/**
+ * Create a Grid from a list of Strings.
+ */
+fun List<String>.toGrid(): Array<CharArray> {
+    return Array(this.size) { r ->
+        val row = this[r]
+        CharArray(row.length) { c -> this[r][c] }
+    }
+}
+
+/**
+ * Get the Manhattan Distance of two points.
+ */
+fun manhattanDistance(a: Pair<Long, Long>, b: Pair<Long, Long>): Long =
+    abs(a.first - b.first) + abs(a.second - b.second)
