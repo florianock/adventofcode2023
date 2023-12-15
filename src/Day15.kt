5ac473e2ec26c366c4fc6db2674db0f09a720976
Florian <florian@zocalo-ict.nl>
2023-12-15 18:42:08 +0100

    some small refactoring

--- src/Day15.kt
+++ src/Day15.kt
@@ -1,32 +1,29 @@
-fun main() {
-    fun hash(s: String): Int =
-        s.fold(0) { currentValue, char -> ((char.code + currentValue) * 17) % 256 }
+fun main() { // --- Day 15: Lens Library ---
 
-    fun part1(input: List<String>): Int = input.single().splitToSequence(",").map { hash(it) }.sum()
+    fun parse(input: List<String>) = input.single().splitToSequence(",")
 
-    fun part2(input: List<String>): Int {
-        val boxes: MutableMap<Int, MutableMap<String, Int>> = mutableMapOf()
-        input.single().splitToSequence(",").forEach { step ->
+    fun hash(s: String) = s.fold(0) { currentValue, char -> ((char.code + currentValue) * 17) % 256 }
+
+    fun getBoxes(input: List<String>): MutableMap<Int, MutableMap<String, Int>> {
+        val boxes = mutableMapOf<Int, MutableMap<String, Int>>()
+        parse(input).forEach { step ->
             val (label: String, op: String, focalLength: String) = """(\w+)([=|-])(\d?)""".toRegex()
                 .find(step)?.destructured ?: throw RuntimeException("Unexpected step: $step")
             val box = hash(label)
             when (op) {
-                "=" -> {
-                    val newBoxMap = boxes.getOrDefault(box, mutableMapOf())
-                    newBoxMap[label] = focalLength.toInt()
-                    boxes[box] = newBoxMap
-                }
-
+                "=" -> boxes.getOrPut(box) { mutableMapOf() }[label] = focalLength.toInt()
                 "-" -> boxes[box]?.remove(label)
                 else -> throw RuntimeException("Unexpected operation: $op")
             }
         }
-        return boxes.also(::println).map { (box, slots) ->
-            slots.toList()
-                .mapIndexed { slot, (_, focalLength) -> (1 + box) * (slot + 1) * focalLength }
-        }
-            .flatten().sum()
+        return boxes
     }
+
+    fun part1(input: List<String>) = parse(input).sumOf { hash(it) }
+
+    fun part2(input: List<String>) = getBoxes(input).map { (box, slots) ->
+        slots.toList().mapIndexed { slot, (_, focalLength) -> (1 + box) * (slot + 1) * focalLength }
+    }.flatten().sum()
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day15_test")
